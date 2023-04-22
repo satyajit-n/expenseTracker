67aba4bcbb671b0fdf6a9354313a433807680e84
satyajit-n <namawar.satyajeet@gmail.com>
2023-04-22 11:04:32 +0530

    Expense Tracker Front End

---
+++ main.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector(".btn");
+const myForm = document.querySelector("#my-form");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
+const delButton = document.querySelector(".btn:hover");
+
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  let myObj = {
+    name: name,
+    email: email,
+    password: password,
+  };
+
+  axios
+    .post("http://localhost:3000/expense-user/add-user", myObj)
+    .then((res) => {
+      window.location.reload();
+      console.log(res);
+    })
+    .catch((err) => {
+      document.body.innerHTML =
+        document.body.innerHTML + "<h2>Something Went wrong<h2/>";
+      console.log(err);
+    });
+}
