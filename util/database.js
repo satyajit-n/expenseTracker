a7127be12ce69b24523d2749b6c66a8c9c192ee6
satyajit-n <namawar.satyajeet@gmail.com>
2023-07-06 12:43:23 +0530

    final COMMIT

--- util/database.js
+++ util/database.js
@@ -1,12 +1,12 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USER}`,
-  `${process.env.DB_PASSWORD}`,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     dialect: "mysql",
-    host: "Localhost",
+    host: process.env.DB_HOST,
   }
 );
 
